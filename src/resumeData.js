757bb1a4475eafdc65d2f9113236aa225ff345b2
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2023-01-16 15:33:27 +1100

    Added resume

---
+++ src/resumeData.js
@@ -0,0 +1,66 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Izzy Khalifa",
+    "role": "FullStack developer",
+    "linkedinId":"Ismail Khalifa",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/izzy-khalifa/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/IzzyKhalifa",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I studied CS for 2 years in collage then dropped out and enrolled in Fullstack development at Monash University As a junior full stack developer, I possess a unique set of skills that allow me to work on both the front-end and back-end of web development projects. I have a strong understanding of programming languages such as HTML, CSS, JavaScript and frameworks such as React, as well as experience with back-end languages like Java, and Node.js. I'm able to design and develop responsive and dynamic web applications that meet the needs of my clients and users. My attention to detail and problem-solving abilities make you a valuable asset toany development team. ",
+    "address":"Melbourne",
+    "website":"https://github.com/IzzyKhalifa",
+    "education":[
+      {
+        "UniversityName":"Monash University",
+        "specialization":"Fullstack",
+        "MonthOfPassing":"Feb",
+        "YearOfPassing":"2023",
+        "Achievements":"Some Achievements"
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Handlebars"
+      },
+      {
+        "skillname":"Mysql"
+      },
+      {
+        "skillname":"mongodb"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Junior Jobs",
+        "imgurl":""
+      },
+      {
+        "name":"project2",
+        "description":"Tech Blog",
+        "imgurl":""
+      },
+    ],
+  }
+  
+  export default resumeData
